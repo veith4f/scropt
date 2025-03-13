cadebe0992b53db8f41f3398ab9e96550264aef6
Fabian Schuetz <fabian.schuetz@plusserver.com>
2025-03-13 14:28:30 +0100

    fix test

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -26,7 +26,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/format"
+	"github.com/onsi/gomega/format"
 
 	"github.com/veith4f/scropt/test/utils"
 )
